e02c1d62e4a06d34d2d1f7b71b963dd4286eb0c5
Talysson <talyssoncastro@gmail.com>
2020-11-23 16:10:25 -0300

    feat: Custom exception to handle invalid scores

---
+++ src/main/java/com/jobsity/talyssondecastro/bowling/score/exception/InvalidScoreException.java
@@ -0,0 +1,12 @@
+package com.jobsity.talyssondecastro.bowling.score.exception;
+
+/**
+ * Created by talyssoncastro on 23/11/2020 3:52 PM.
+ */
+public class InvalidScoreException extends Exception {
+
+    public InvalidScoreException(String errorMessage) {
+        super(errorMessage);
+    }
+
+}
